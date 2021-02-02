06742ef8e26ff821d18ab42dce2b3a7216a0eda0
WithAJoint <WithAJoint@protonmail.com>
2021-02-02 14:08:03 +0100

    begin configuration rest api

--- rest-api/src/main/java/network/thezone/routiner/rest/resources/Activities.java
+++ rest-api/src/main/java/network/thezone/routiner/rest/resources/Activities.java
@@ -1,4 +1,18 @@
 package network.thezone.routiner.rest.resources;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("activities")
 public class Activities {
+
+    public Activities() {
+    }
+
+    @GET
+    @Produces("text/json")
+    public String getActivities() {
+        return "test";
+    }
 }
