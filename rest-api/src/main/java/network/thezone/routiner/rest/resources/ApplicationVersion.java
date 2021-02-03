0552b7323e8ee557a3196acf161039dfc3d524be
WithAJoint <WithAJoint@protonmail.com>
2021-02-03 10:43:06 +0100

    fix liberty config folder misplacement

---
+++ rest-api/src/main/java/network/thezone/routiner/rest/resources/ApplicationVersion.java
@@ -0,0 +1,8 @@
+package network.thezone.routiner.rest.resources;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("v1")
+public class ApplicationVersion extends Application {
+}
